c35feac0c73df0b9e7bb811d02979a1e24bf5b49
erickbxs <erickbxs@mgmail.com>
2023-07-17 19:36:36 -0300

    Feat: Req 08 - func word_search(search_by_wrod)

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -23,4 +23,25 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for index in range(len(instance)):
+        file_data = instance.search(index)
+        file_name = file_data["nome_do_arquivo"]
+        file_lines = file_data["linhas_do_arquivo"]
+
+        occurrences = []
+        for line_num, line in enumerate(file_lines, start=1):
+            if word.lower() in line.lower():
+                line_info = {"linha": line_num, "conteudo": line.strip()}
+                occurrences.append(line_info)
+
+        if occurrences:
+            file_info = {
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": occurrences,
+            }
+            result.append(file_info)
+
+    return result
