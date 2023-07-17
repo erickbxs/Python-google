5c1c5adad5e81a0cdd2047fff30205cec25ade52
erickbxs <erickbxs@mgmail.com>
2023-07-17 14:47:04 -0300

    Feat: Requisito 01 - Fila p arm de arq

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,23 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.is_empty():
+            raise IndexError("A fila está vazia")
+        return self.queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index < len(self.queue):
+            return self.queue[index]
+        raise IndexError("Índice Inválido ou Inexistente")
+
+    def is_empty(self):
+        return len(self.queue) == 0
