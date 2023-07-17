13509a0bf922e7aa74939ade1b3cee49f277b3d4
erickbxs <erickbxs@mgmail.com>
2023-07-17 15:45:34 -0300

    Feat: Requisito 03 - func process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,28 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    # Verifica se o arquivo já foi processado anteriormente
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return
+
+    # Importa as linhas do arquivo
+    file_lines = txt_importer(path_file)
+
+    # Cria o dicionário com os dados do arquivo
+    file_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_lines),
+        "linhas_do_arquivo": file_lines,
+    }
+
+    # Adiciona o dicionário à fila
+    instance.enqueue(file_data)
+
+    # Exibe os dados processados via stdout
+    sys.stdout.write(str(file_data))
 
 
 def remove(instance):
