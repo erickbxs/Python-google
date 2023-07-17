61c28d4592af4b67aa01877f1d019dd09f0d5792
erickbxs <erickbxs@mgmail.com>
2023-07-17 19:33:12 -0300

    Feat: Requisito 07 - func word_search(exist_word)

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for index in range(len(instance)):
+        file_data = instance.search(index)
+        file_name = file_data["nome_do_arquivo"]
+        file_lines = file_data["linhas_do_arquivo"]
+
+        occurrences = []
+        for line_num, line in enumerate(file_lines, start=1):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": line_num})
+
+        if occurrences:
+            file_info = {
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": occurrences,
+            }
+            result.append(file_info)
+
+    return result
 
 
 def search_by_word(word, instance):
